c346beb8e58fe1238b0a3c56fab633651058dcd9
Angiefiona <angellamumoh@gmail.com>
2023-07-04 21:06:39 +0300

    first commit

---
+++ src/Login/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Login =() =>{
+    return(
+        <div>
+        <form>
+        <h1>Login</h1>
+        <input placeholder="Enter Username" type="text"/>
+        <br/>
+        <br/>
+        <input placeholder="Enter password"type="text"/>
+        <br/>
+        <br/>
+        <button>Login</button>
+        </form>
+
+
+        </div>
+      
+      )
+    }
+   
+    
+export default Login;
